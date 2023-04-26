269c66083d2bb691de9f3d86593f64e71882c307
Fabriciooo <fabricerob@yahoo.fr>
2023-04-26 18:39:21 +0200

    commit

---
+++ Cards-master/src/pages/About.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+const About = () => {
+    return (
+        <div>
+            <Logo/>
+            <Navigation/>
+            <h1>A propos</h1>
+            <br />
+            <p>Lors de la séance consacrée à l’étude des combattants pendant la Grande Guerre,
+les élèves sont amenés à travailler sur un corpus de documents consacrés à la
+bataille de la Somme sans questionnaire. Une boîte à outils, constituée par un
+questionnaire et du vocabulaire, est toutefois laissée à disposition des élèves qui le
+souhaitent.
+La consigne donnée est : « après avoir relevé les principales informations que vous
+pouvez retenir de ces documents, écrivez la lettre d’un poilu durant cette bataille à
+un destinataire (librement choisi) pour évoquer les conditions de vie, de combat et
+                interroger le destinataire sur son quotidien à l’arrière. »
+            </p>
+            <p>
+            Lors de la séance consacrée à l’étude des combattants pendant la Grande Guerre,
+les élèves sont amenés à travailler sur un corpus de documents consacrés à la
+bataille de la Somme sans questionnaire. Une boîte à outils, constituée par un
+questionnaire et du vocabulaire, est toutefois laissée à disposition des élèves qui le
+souhaitent.
+La consigne donnée est : « après avoir relevé les principales informations que vous
+pouvez retenir de ces documents, écrivez la lettre d’un poilu durant cette bataille à
+un destinataire (librement choisi) pour évoquer les conditions de vie, de combat et
+interroger le destinataire sur son quotidien à l’arrière. »
+            </p>
+        </div>
+    );
+};
+
+export default About;
